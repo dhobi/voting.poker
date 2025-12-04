f85d94dce1449b9369a85acd0cfcc88d1c17b4af
Daniel Hobi <info@danielhobi.ch>
2025-12-04 13:22:34 +0100

    Update constants.ts
    process not available in frontend ;-)

--- src/constants.ts
+++ src/constants.ts
@@ -7,10 +7,10 @@ export const siteHost = isDev
   ? typeof location !== "undefined"
     ? location.host
     : "localhost:3000"
-  : process.env.HOSTNAME;
+  : 'voting.danielhobi.ch'; //process.env.HOSTNAME not given, used in frontend directly
 
 export const BASE_URL = isDev ? `http://${siteHost}` : `https://${siteHost}`;
 
-export const ablyKey = process.env.ABLYKEY;
+export const ablyKey = 'HwJv2w.DsN0YA:oTLUBQ1wv_4HEl-BGzyzmpHVY8b65_xpdEErHGjcMZM'; //process.env.ABLYKEY not given, used in frontend directly
 
-export const giphyKey = process.env.GIPHYKEY;
+export const giphyKey = 'ryZAHa7ppKnf7QJaIuxA5ws2BRPVNyMp'; //process.env.GIPHYKEY not given, used in frontend directly
